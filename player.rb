f2618128f22d5df9e3749ae652d6de2874015fb6
Layne Rasmussen <layrasm@gmail.com>
2020-08-24 08:42:52 -0600

    player profile start

--- player.rb
+++ player.rb
@@ -1,3 +1,15 @@
 class Player
+ attr_accessor :name, :wallet
 
+ def initialize(name, wallet)
+  @name = name
+  @wallet = wallet
+  end
 end
+
+puts "Please enter your name"
+player_name = gets.chomp
+player = player.new(player_name, 1000)
+
+puts "Welcome #{player_name}"
+
