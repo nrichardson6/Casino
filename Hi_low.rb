b3f52435b9cc5526057aa386041eceb2dfdc0d64
Layne Rasmussen <layrasm@gmail.com>
2020-08-24 08:22:37 -0600

    simple hi-lo

--- Hi_low.rb
+++ Hi_low.rb
@@ -1,3 +1,17 @@
-class High_low
-
-end
+def game
+    num = rand 25
+    puts "Guess a number between 0 and 24"
+    
+    loop do
+      user_answer = gets.chomp.to_i
+  
+      if user_answer == num
+        puts "you got it right"
+        break
+      elsif user_answer > num
+        puts "Guesssed too  high, try a lower number"
+      elsif user_answer < num
+        puts "You guessed too low, guess higher number"
+      end
+    end
+  end
